aa94992e9d5a60e70d466be757bb31acd1d2a6d2
DimitriusJeffries <raelfilms@gmail.com>
2019-12-11 10:00:01 -0600

    finished build-a-blog

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'asdf123cvxc'
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/new-post', methods=['POST', 'GET'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        blog_post = Blog(title, body)
+        db.session.add(blog_post)
+        db.session.commit()
+        return render_template('viewpost.html', blog=blog_post)
+
+    return render_template('new-post.html')
+
+@app.route('/blog', methods=['GET'])
+def blog():
+    blog_id = request.args.get('id')
+    if blog_id:
+        blog = Blog.query.get(blog_id)
+        return render_template('viewpost.html', blog=blog)
+   
+   
+   
+    blogs = Blog.query.all()
+    return render_template("main-page.html", blogs=blogs)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
